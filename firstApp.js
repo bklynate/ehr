23b7a333f171213b16cc7aefc99a844ad222ec5b
Dimeji Abidoye <ota2@duke.edu>
2015-12-01 03:35:52 -0500

    Create firstApp.js

---
+++ firstApp.js
@@ -0,0 +1,17 @@
+var url = "http://localhost:8000/all";                                                                                                                                               
+                                                                                                       
+angular.module('firstApp', [])                                                                         
+                                                                                                       
+.controller('mainController', [ '$scope', '$http', function($scope, $http) {                           
+    // bind this to vm (view-model)                                                                    
+    // define variables and objects on this                                                            
+    // this lets them be available to our views                                                        
+    // define a list of items                                                                          
+                                                                                                       
+                                                                                                       
+  $scope.players = function(){                                                                         
+       console.log("here");                                                                            
+       $http.get(url).success(function(response){ return response.items });                            
+   }                                                                                                   
+                                                                                                       
+}]);            
